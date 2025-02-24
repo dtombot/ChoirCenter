a5fcd5b27a0aa4da1e2523773919a56dbf37273f
dtombot <tomselfdevelopr@gmail.com>
2025-02-24 20:04:43 +0100

    Create Privacy.jsx

---
+++ src/pages/Privacy.jsx
@@ -0,0 +1,10 @@
+function Privacy() {
+  return (
+    <div className="container">
+      <h2>Privacy Policy</h2>
+      <p>At Choir Center, we value your privacy. Your personal information is used only to enhance your experience and is never shared without consent.</p>
+    </div>
+  );
+}
+
+export default Privacy;

fca0e886ece7c49264d8b3f54f0845d4eaec1c6d
dtombot <tomselfdevelopr@gmail.com>
2025-02-24 20:05:12 +0100

    Create Terms.jsx

---
+++ src/pages/Terms.jsx
@@ -0,0 +1,10 @@
+function Terms() {
+  return (
+    <div className="container">
+      <h2>Terms of Service</h2>
+      <p>By using Choir Center, you agree to use our resources responsibly and respect the intellectual property of the music provided.</p>
+    </div>
+  );
+}
+
+export default Terms;

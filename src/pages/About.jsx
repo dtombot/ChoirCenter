aa77acc84f934289df195f9b891258836156dbdd
dtombot <tomselfdevelopr@gmail.com>
2025-02-24 20:03:43 +0100

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div className="container">
+      <h2>About Us</h2>
+      <p>Choir Center is dedicated to supporting choristers worldwide by providing easy access to choir music resources. Our mission is to preserve and share the beauty of choral music.</p>
+    </div>
+  );
+}
+
+export default About;

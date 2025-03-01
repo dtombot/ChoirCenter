c005511e30f67204e760ac08a377f067f9c0b3e9
dtombot <tomselfdevelopr@gmail.com>
2025-03-01 02:52:31 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../supabase';
 import { Link, useNavigate } from 'react-router-dom';
+import AdBanner from '../components/AdBanner';
 import '../styles.css';
 
 function Home() {
@@ -187,16 +188,7 @@ function Home() {
           </div>
         </div>
       </section>
-      <aside className="ad-space">
-        <div className="ad-sample">
-          <span className="ad-text">Place your Ad here. Advertise on ChoirCenter.com</span>
-          <svg className="ad-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path fill="#3cb371" d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16z"/>
-            <path fill="#fff" d="M12 6l4 6h-8l4-6zm0 6v6h-2v-6h2z"/>
-          </svg>
-          <a href="mailto:admin@choircenter.com" className="ad-link">Contact Us</a>
-        </div>
-      </aside>
+      <AdBanner position="home_above_sotw" />
       <section className="song-of-the-week">
         <h2 className="section-title animate-text">Song of the Week</h2>
         {songOfTheWeek ? (
